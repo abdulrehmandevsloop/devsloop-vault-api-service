1fdb39d7c38c2292c5577d14c879b7233fa0eadf
aqibali <aqib@devslooptech.com>
2026-02-05 17:51:26 +0500

    feat: deployment files added

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'admin@devsloop.com' })
+  @ApiProperty({ example: 'aqib@devslooptech.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
